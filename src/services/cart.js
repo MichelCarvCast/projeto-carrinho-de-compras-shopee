d415f5616855c7ce1373b0810e2b304306dc4e98
MichelCarvCast <02michelc@gmail.com>
2024-08-29 11:48:42 -0300

    AddItem e CalculateTotal implementados

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,8 +1,8 @@
 //Quais ações meu carrinho pode fazer
 
 // adicionar item no cart
-async function addItem(userCart) {
-    
+async function addItem(userCart, item) {
+    userCart.push(item);
 }
 
 // deletar item do cart
@@ -17,5 +17,13 @@ async function removeItem(userCart, index){
 
 // calcular o total
 async function calculateTotal(userCart) {
-    
+    return userCart.reduce((total, item) => total + item.subtotal(), 0);
+}
+
+
+export {
+    addItem,
+    deleteItem,
+    removeItem,
+    calculateTotal
 }
