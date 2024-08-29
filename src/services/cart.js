478023117a5407dce9c325219029329290f35f7e
MichelCarvCast <02michelc@gmail.com>
2024-08-29 11:12:00 -0300

    Iniciando o desenvolvimento do Cart

---
+++ src/services/cart.js
@@ -0,0 +1,21 @@
+//Quais ações meu carrinho pode fazer
+
+// adicionar item no cart
+async function addItem(userCart) {
+    
+}
+
+// deletar item do cart
+async function deleteItem(userCart, name){
+
+}
+
+// remover um item
+async function removeItem(userCart, index){
+
+}
+
+// calcular o total
+async function calculateTotal(userCart) {
+    
+}
